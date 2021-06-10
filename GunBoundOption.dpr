275a282762d79304a8b357e8c3e4d4061f7280e5
thiagostmarquezini@gmail.com <thiago-marquezini>
2021-06-10 14:12:03 -0300

    deleted unused files

--- GunBoundOption.dpr
+++ GunBoundOption.dpr
@@ -4,8 +4,6 @@ uses
   Forms,
   uGameOptions in 'uGameOptions.pas' {frmOptions},
   ExtFunctions in 'ExtFunctions.pas',
-  GlobalVars in 'GlobalVars.pas',
-  tInterfaceRefresher in 'tInterfaceRefresher.pas',
   Vcl.Themes,
   Vcl.Styles;
 
